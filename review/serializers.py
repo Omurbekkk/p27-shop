3bbf0960ce6d92232d6c89ec8778f7e5acbc8800
Omurbek <odosumbekov@gmail.com>
2023-04-28 17:35:07 +0600

    search + filters + spam

--- review/serializers.py
+++ review/serializers.py
@@ -12,6 +12,14 @@ class CommentSerializer(ModelSerializer):
         attrs["user"] = self.context["request"].user
         return attrs
 
+    def to_representation(self, instance: Comment):
+        rep = super().to_representation(instance)
+        rep["user"] = {
+            "id": instance.user.id,
+            "email": instance.user.email
+            }
+        return rep
+
 
 class RatingSerializer(ModelSerializer):
     class Meta:
@@ -41,5 +49,12 @@ class FavouriteSerializer(ModelSerializer):
         super().validate(attrs)
         attrs["user"] = self.context["request"].user
         return attrs
+
+    def to_representation(self, instance: Favourite):
+        from main.serializers import ProductSerializer
+
+        rep = super().to_representation(instance)
+        rep["product"] = ProductSerializer(instance.product).data
+        return rep
 
 
